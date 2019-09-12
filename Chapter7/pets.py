571d48b2ef3c629365d412aaa50f99f0d1e02cc3
ashish <achaturvedi@agmednet.com>
2019-09-12 14:57:13 -0400

    Adding pets.py

---
+++ Chapter7/pets.py
@@ -0,0 +1,11 @@
+'''
+Created on Sep 12, 2019
+
+@author: achaturvedi
+'''
+pets = ['dos', 'cat', 'hosrse', 'rat', 'cat', 'elephant']
+
+while 'cat' in pets:
+    pets.remove('cat')
+    
+print(pets)

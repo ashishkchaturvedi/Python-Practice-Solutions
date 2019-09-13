e6743e4a7caabe61e1c0fccc307f610d04d9e9b2
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-09-13 10:32:15 -0400

    test

---
+++ Chapter8_Functions/magicians.py
@@ -0,0 +1,13 @@
+'''
+Created on Sep 13, 2019
+
+@author: achaturvedi
+'''
+def shoe_magicians(magicians):
+    for magician in magicians:
+        print("\n" + magician.title())
+        
+        
+magicians = ['sarkar', 'majumdar', 'clinton']
+
+shoe_magicians(magicians)

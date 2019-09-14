8068276fd257f68d52f5cb82555d84bf159afb49
ashishkchaturvedi <ashishkchaturvedi@gmail.com>
2019-09-14 19:58:42 -0400

    Adding electric car to Car class

---
+++ Chapter9_Classes/electric_car.py
@@ -0,0 +1,69 @@
+'''
+Created on Sep 14, 2019
+
+@author: achaturvedi
+'''
+from builtins import str
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 23
+        
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+    
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+        
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+        
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back the value")
+        
+class ElectricCar(Car):
+    
+    def __init__(self, make, model, year):
+        self.battery_size = 70
+        
+        super().__init__(make, model, year)
+        self.battery = Battery(85)
+        
+    '''def describe_battery(self):
+        print("This car has a " +str(self.battery_size) + "-kWh battery.")'''
+        
+        
+class Battery():
+    def __init__(self, battery_size):
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+        
+    def get_range(self):
+        
+        if self.battery_size == 70:
+            car_range = 240
+        elif self.battery_size == 85:
+            car_range = 270
+        message = "This car can go approx. " + str(car_range)
+        message += " miles on a full charge."
+    
+        print(message)
+        
+my_tesla = ElectricCar('tesla', 'model s' , '2019')
+
+new_car = my_tesla.get_descriptive_name()
+
+print(new_car)
+
+#my_tesla.describe_battery()
+
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
